afbe56b8e150b1c8da7b903ddfc007a20eb55daa
Roman Popov <i@romanpopoff.ru>
2023-05-01 13:41:01 +0300

    создан функционал корзины

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls', namespace='shop'))
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
